98339685b80746ead29fe443af0ef5627960a857
Vu Diep <Vu Diep>
2017-04-11 18:20:24 +0700

    Update hopDongNganHan.js

--- src/main/webapp/resources/js/hopDongNganHan.js
+++ src/main/webapp/resources/js/hopDongNganHan.js
@@ -1,144 +1,168 @@
 $(document).ready(function() {
-    var hopDongNganHanService = "/hopDongNganHanService";
-    var hopDongNganHanController = "/hopDongNganHanController";
-    var table = $('#HopDongNganHanTable').DataTable({
-        "sAjaxSource": "/hopDongNganHanService" +
-            "/getAll",
-        "sAjaxDataProp": "",
-        "order": [
-            [0, "asc"]
-        ],
-        "aoColumnDefs": [{
-                "aTargets": [0],
-                "mData": "tenHopDong"
-            },
-            {
-                "aTargets": [1],
-                "mData": "tenNhanVien"
-            },
-            {
-                "aTargets": [2],
-                "mData": "ngayKy"
-            },
-            {
-                "aTargets": [3],
-                "mData": "tuNgay"
-            },
-            {
-                "aTargets": [4],
-                "mData": "denNgay"
-            },
-            {
-                "targets": -1,
-                "data": null,
-                "defaultContent": "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' id='btnDel'>Xóa</button>"
-            },
-            {
-                "targets": -2,
-                "data": null,
-                "defaultContent": "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' id='btnUpdate'>Sửa</button>"
-            },
-        ],
-        scrollY: "600px",
-        scrollCollapse: true,
-        dom: 'Blfrtip',
-        buttons: [
-            'excel',
-            'pdf',
-            'print',
-            {
-                text: 'THÊM HỢP ĐỒNG NGẮN HẠN',
-                action: function(e, dt, node, config) {
-                    $('#formHDNganHan').modal('show');
-                }
+		var hopDongNganHanService = "/hopDongNganHanService";
+		var hopDongNganHanController = "/hopDongNganHanController";
+		var table = $('#HopDongNganHanTable').DataTable({
+			"sAjaxSource" : hopDongNganHanService+"/getAll",
+			"sAjaxDataProp" : "",
+			"order" : [ [ 0, "asc" ] ],
+			"aoColumnDefs": [ 
+			      {
+				       "aTargets": [ 0 ],
+				       "mData": "tenHopDong"
+			      },
+			      {
+					   "aTargets": [ 1 ],
+					   "mData": "tenNhanVien"
+				  },
+				  {
+					   "aTargets": [ 2 ],
+					   "mData": "ngayKy"
+				  },
+				  {
+					   "aTargets": [ 3 ],
+					   "mData": "tuNgay"
+				  },
+				  {
+					   "aTargets": [ 4 ],
+					   "mData": "denNgay"
+				  },
+				  {
+				       "targets": -1,
+				       "data": null,
+				       "defaultContent": "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' id='btnDel'>Xóa</button>"
+			      },
+			      {
+					   "targets": -2,
+					   "data": null,
+					   "defaultContent": "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' id='btnUpdate'>Sửa</button>"
+				  },
+			],
+			scrollY : "600px",
+			scrollCollapse: true,
+			dom: 'Blfrtip',
+			buttons: [
+			          'excel',
+			          'pdf',
+			          'print',
+			          {
+		                 text: 'THÊM HỢP ĐỒNG NGẮN HẠN',
+		                 action: function ( e, dt, node, config ) {
+		                	 var txtTenHopDong=$(tenhopdong);
+		                     var txtTenNhanVien=$(tennhanvien);
+		                     var txtNgayKy=$(dpNgayKy);
+		                     var txtTuNgay=$(dpTuNgay);
+		                     var txtDenNgay=$(dpDenNgay);
+		                     
+		                     	 txtTenHopDong.val("");
+	                         	 txtTenNhanVien.val("");
+	                         	 txtNgayKy.val("");
+	                         	 txtTuNgay.val("");
+	                         	 txtDenNgay.val("");
+		                     
+		                     $('#formHDNganHan').modal('show');
+		                 },
+		              }
+			          ],
+		    colReorder: true,
+		    select: true,
+		    "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.10.13/i18n/Vietnamese.json"
             }
-        ],
-        colReorder: true,
-        select: true,
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.13/i18n/Vietnamese.json"
-        }
-    });
-
-    $('#HopDongNganHanTable tbody').on('click', 'button', function() {
-        var id = $(this)[0].id;
-        if ("btnDel" == id) {
-            var data = table.row($(this).parents('tr')).data();
-            check = confirm("Bạn có chắc chắn muốn xóa đối tượng : " + data['tenNhanVien']);
-            var maHDNganHan = data['maHDNganHan'];
-            if (check == true) {
-                $.ajax({
-                    url: hopDongNganHanController + "/delete/" + maHDNganHan,
-                    type: 'DELETE',
-                    success: function(res) {
-                        table.ajax.reload();
-                    }
-                });
-            }
-        }
-    });
-
-    $('#HopDongNganHanTable tbody').on('click', 'button', function() {
-        var id = $(this)[0].id;
-        if ("btnUpdate" == id) {
-            var data = table.row($(this).parents('tr')).data();
-            var maHDNganHan = data['maHDNganHan'];
-            $.ajax({
-                url: "/hopDongNganHanService" + "/getById/" + maHDNganHan,
-                type: 'GET',
-                success: function(res) {
-                    txtTenHopDong.val(res.tenHopDong);
-                    txtTenNhanVien.val(res.tenNhanVien);
-                    txtNgayKy.val(res.ngayKy);
-                    txtTuNgay.val(res.tuNgay);
-                    txtDenNgay.val(res.denNgay);
-                    $('#formHDNganHan').modal('show');
-                }
-            });
-        }
-    });
-
-    $("button#btnCapNhap").click(function(e) {
-    	
-    	var endpointUrl = '/hopDongNganHanController/update';
-    	
-    	// TODO: need to implement code @here
-    	// if(dang la Add new) {
-    	// endpointUrl = '/hopDongNganHanController/add';
-    	// }
-    	
-    	var txtTenHopDong = $(tenhopdong);
-        var txtTenNhanVien = $(tennhanvien);
-        var txtNgayKy = $(dpNgayKy);
-        var txtTuNgay = $(dpTuNgay);
-        var txtDenNgay = $(dpDenNgay);
-        
-        var json = new Object();
-        // TODO: don't need the valud of [maHDNganHan] @here because user doesn't input this value
-        json.maHDNganHan = -1;
-        json.tenHopDong = txtTenHopDong.val();
-        json.tenNhanVien = txtTenNhanVien.val();
-        json.ngayKy = txtNgayKy.val();
-        json.tuNgay = txtTuNgay.val();
-        json.denNgay = txtDenNgay.val();
-        
-        $.ajax({
-            type: "POST",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(json),
-            url: endpointUrl,
-            success: function(msg) {
-                table.ajax.reload();
-            },
-            error: function() {
-                alert("Failed");
-            }
+		});
+	
+		$('#HopDongNganHanTable tbody').on( 'click', 'button', function () {
+			var id = $(this)[0].id;
+			if("btnDel" == id){
+	        var data = table.row($(this).parents('tr')).data();
+	        check = confirm("Bạn có chắc chắn muốn xóa đối tượng : "
+                    + data['tenNhanVien'])
+                    var maHDNganHan = data['maHDNganHan'];
+	            if(check==true){
+	            	$.ajax({  
+	                    url: hopDongNganHanController+"/delete/"+maHDNganHan,  
+	                    type: 'DELETE',  
+	                    success: function (res) {
+	                    	alert("Xóa Thành Công");
+	                    	table.ajax.reload();	                    }  
+	                });
+	            }
+                    
+			}
+	    });
+		
+		$('#HopDongNganHanTable tbody').on( 'click', 'button', function () {
+			var id = $(this)[0].id;
+			if("btnUpdate" == id){
+	        var data = table.row($(this).parents('tr')).data();
+                    var maHDNganHan = data['maHDNganHan'];
+                    //$(tenhopdong) giá trị ban đầu : Rỗng
+                    var txtTenHopDong=$(tenhopdong);
+                    var txtTenNhanVien=$(tennhanvien);
+                    var txtNgayKy=$(dpNgayKy);
+                    var txtTuNgay=$(dpTuNgay);
+                    var txtDenNgay=$(dpDenNgay);
+	            	$.ajax({  
+	                    url: hopDongNganHanService+"/getById/"+maHDNganHan,  
+	                    type: 'GET',  
+	                    success: function (res) {
+	                    	//alert(res.tenNhanVien);
+	                    	//tenhopdong: id cua the input, res.tenHopDong : lay tu chuoi json dc get tu co do du lieu
+	                    	//lay thong tin tu co so du lieu gan nen form
+	                    	txtTenHopDong.val(res.tenHopDong);
+	                    	txtTenNhanVien.val(res.tenNhanVien);
+	                    	txtNgayKy.val(res.ngayKy);
+	                    	txtTuNgay.val(res.tuNgay);
+	                    	txtDenNgay.val(res.denNgay);
+		                    $('#formHDNganHan').modal('show');
+	                    }
+	                });
+			}	
+	    });
+		
+		//twitter bootstrap btnCapNhap
+    	$("button#btnCapNhap").click(function(e) {
+    		
+    		var endpointUrl = '/hopDongNganHanController/update';
+    		//$(tenhopdong) là giá trị sau khi lấy ở database nên gắn vào form khi click nút Sửa
+    		var txtTenHopDong = $(tenhopdong);
+            var txtTenNhanVien = $(tennhanvien);
+            var txtNgayKy = $(dpNgayKy);
+            var txtTuNgay = $(dpTuNgay);
+            var txtDenNgay = $(dpDenNgay);
+            
+         	var json = new Object();
+            json.maHDNganHan = 7;
+            json.tenHopDong = txtTenHopDong.val();
+            json.tenNhanVien = txtTenNhanVien.val();
+            json.ngayKy = txtNgayKy.val();
+            json.tuNgay= txtTuNgay.val();
+            json.denNgay = txtDenNgay.val();
+         	$.ajax({
+                 type : "POST",
+                 contentType: "application/json; charset=utf-8",
+                 data : JSON.stringify(json),
+                 url : endpointUrl,
+                 success : function(msg) {
+                     	table.ajax.reload();
+                  },
+                  error : function() {
+                        alert("Sửa không thành công");
+                  }
+             });
         });
-    });
-});
-
-/*  datepicker*/
-$.fn.datepicker.defaults.format = "yyyy-mm-dd";
-$('.datepicker').datepicker({
-    startDate: '-3d'
-});
+    	
+    	$("button#btnDong").click(function(e) {
+         	txtTenHopDong.val("");
+         	txtTenNhanVien.val("");
+         	txtNgayKy.val("");
+         	txtTuNgay.val("");
+         	txtDenNgay.val("");
+        }); 
+		
+	} );
+	
+	/*  datepicker*/
+	$.fn.datepicker.defaults.format = "yyyy-mm-dd";
+		$('.datepicker').datepicker({
+			 	startDate: '-3d'
+	});
